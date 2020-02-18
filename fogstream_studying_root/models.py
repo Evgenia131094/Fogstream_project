e5ef4c63ffa323dd88c6bfab6a9655e08ded8ba7
Evgenia131094 <evgenia131094@mail.ru>
2020-02-18 19:59:02 +1000

    added url root var var var

--- fogstream_studying_root/models.py
+++ fogstream_studying_root/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from datetime import datetime
+from django.core.validators import RegexValidator
 
 
 
@@ -10,20 +11,24 @@ class Subject(models.Model):
     lector = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     lecture_name = models.CharField(max_length=200)
     lecture_description = models.TextField()
-    created_date = models.DateTimeField(default="Asia/Vladivostok")
+    created_date = models.DateTimeField(default=timezone.now)
     lecture_date = models.DateTimeField(blank=True, null=True)
     students_number = models.IntegerField()
 
     def __str__(self):
         # local_t = self.lecture_date.astimezone(Local)
-        return '{0} {1}'.format(self.lecture_name, self.lecture_date.strftime("%d/%m/%Y  в %H:%M"))
+        return '{0} {1}'.format(self.lecture_name, self.lecture_date.strftime("%d/%m/%Y  в %H:%M %z"))
 
 class Student(models.Model):
     participant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20)
-
+    phone_regex = RegexValidator(regex=r'\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}', message="Введенный номер телефона должен быть в следующем формате: '+7(999)999-99-99'")
+    phone_number = phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    
     def __str__(self):
-        return '{1} {0}'.format(self.participant.first_name, self.participant.last_name)
+        name = '{1} {0}'.format(self.participant.first_name, self.participant.last_name)
+        if name == ' ':
+            name = '{}'.format(self.participant.username)
+        return name
 
 class Lecture(models.Model):
     lecture = models.ForeignKey(Subject, on_delete=models.CASCADE)
