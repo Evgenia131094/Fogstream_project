ff9f51941f450b9b15bc2ef1324287929be146ff
Evgenia131094 <33471040+Evgenia131094@users.noreply.github.com>
2020-02-19 15:13:51 +1000

    Update models.py

--- fogstream_studying_root/models.py
+++ fogstream_studying_root/models.py
@@ -20,7 +20,7 @@ class Subject(models.Model):
         return '{0} {1}'.format(self.lecture_name, self.lecture_date.strftime("%d/%m/%Y  в %H:%M %z"))
 
 class Student(models.Model):
-    participant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    participant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'is_staff':False},)
     phone_regex = RegexValidator(regex=r'\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}', message="Введенный номер телефона должен быть в следующем формате: '+7(999)999-99-99'")
     phone_number = phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     
