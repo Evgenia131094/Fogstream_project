e9d84ebe46669a9d4fd52a271e75dec76a6f1cc1
Evgenia131094 <33471040+Evgenia131094@users.noreply.github.com>
2020-02-19 15:18:41 +1000

    Update models.py

--- fogstream_studying_root/models.py
+++ fogstream_studying_root/models.py
@@ -8,7 +8,10 @@ from django.core.validators import RegexValidator
 
 class Subject(models.Model):
     # participant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    lector = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'is_staff':True},)
+    lector = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                               on_delete=models.CASCADE, 
+                               limit_choices_to={'is_staff':True}, 
+                               verbose_name = "Лектор",)
     lecture_name = models.CharField(max_length=200)
     lecture_description = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -20,7 +23,11 @@ class Subject(models.Model):
         return '{0} {1}'.format(self.lecture_name, self.lecture_date.strftime("%d/%m/%Y  в %H:%M %z"))
 
 class Student(models.Model):
-    participant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'is_staff':False},)
+    participant = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                                    on_delete=models.CASCADE, 
+                                    limit_choices_to={'is_staff':False},
+                                    verbose_name = "Студент"
+                                   )
     phone_regex = RegexValidator(regex=r'\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}', message="Введенный номер телефона должен быть в следующем формате: '+7(999)999-99-99'")
     phone_number = phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     
@@ -31,8 +38,12 @@ class Student(models.Model):
         return name
 
 class Lecture(models.Model):
-    lecture = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    student_info = models.ForeignKey(Student, on_delete=models.CASCADE)
+    lecture = models.ForeignKey(Subject, 
+                                on_delete=models.CASCADE, 
+                                verbose_name = "Лекция",)
+    student_info = models.ForeignKey(Student, 
+                                     on_delete=models.CASCADE,
+                                    verbose_name = "Про студента")
 
 
     def __str__(self):
